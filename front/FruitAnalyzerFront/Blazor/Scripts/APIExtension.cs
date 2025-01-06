69ee2a62ba94e6235fb78d5e4d135aafbf146bcf
Dmitriy Zaytsev <dzaytsev308@gmail.com>
2025-01-06 13:02:40 +0300

    Connect front with back

---
+++ front/FruitAnalyzerFront/Blazor/Scripts/APIExtension.cs
@@ -0,0 +1,24 @@
+﻿using FruitAnalyzerFront.AppLogic;
+using static FruitComponent.FruitChart;
+
+namespace FruitAnalyzerFront.Blazor.Scripts
+{
+    public static class APIExtension
+    {
+        public static FruitChartItem[] ToFruitChartItems(this FruitProbabilityData apiData)
+        {
+            if (apiData == null)
+                return Array.Empty<FruitChartItem>();
+
+            var resultChartItems = new List<FruitChartItem>();
+
+            resultChartItems.Add(new FruitChartItem { Fruit = "Banana", Probability = Math.Round(apiData.Banana, 2) });
+            resultChartItems.Add(new FruitChartItem { Fruit = "Apple", Probability = Math.Round(apiData.Apple, 2) });
+            resultChartItems.Add(new FruitChartItem { Fruit = "Grapes", Probability = Math.Round(apiData.Grapes, 2) });
+            resultChartItems.Add(new FruitChartItem { Fruit = "Orange", Probability = Math.Round(apiData.Orange, 2) });
+            resultChartItems.Add(new FruitChartItem { Fruit = "Pineapple", Probability = Math.Round(apiData.Pineapple, 2) });
+
+            return resultChartItems.ToArray();
+        }
+    }
+}
---
+++ front/FruitAnalyzerFront/Blazor/Scripts/FruitChartExtension.cs
@@ -0,0 +1,52 @@
+﻿using static FruitComponent.FruitChart;
+
+namespace FruitAnalyzerFront.Blazor.Scripts
+{
+    public static class FruitChartExtension
+    {
+        public static string[] ToColorArray(this FruitChartItem[] fruitChartItems)
+        {
+            if (fruitChartItems == null)
+                return Array.Empty<string>();
+
+            var resultColors = new List<string>();
+
+            foreach (var item in fruitChartItems)
+            {
+                switch (item.Fruit.ToLower())
+                {
+                    case "banana":
+                        resultColors.Add("#fffbc9");
+                        break;
+                    case "apple":
+                        resultColors.Add("#8b0202");
+                        break;
+                    case "grapes":
+                        resultColors.Add("#abe16b");
+                        break;
+                    case "orange":
+                        resultColors.Add("#ffa500");
+                        break;
+                    case "pineapple":
+                        resultColors.Add("#ffbd35");
+                        break;
+                    default:
+                        throw new Exception("Can't recognize fruit color!");
+                }
+            }
+
+            return resultColors.ToArray();
+        }
+
+        public static FruitChartItem FindFruitWithMaxProbability(this FruitChartItem[] chartItems)
+        {
+            if (chartItems == null || chartItems.Length == 0)
+                throw new Exception("Can't find fruit in empty list!");
+
+            // Find max by comparing all fruits
+            var maxChartItem = chartItems.Aggregate((max, current) => current.Probability > max.Probability ? current : max);
+
+            return maxChartItem;
+        }
+    }
+}
