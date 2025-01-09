6233c0957b1d3f291de4f2013275241414766b2d
AlexShmak <lexnder9@gmail.com>
2025-01-09 13:56:26 +0300

    fix model loading function

--- back/src/model.py
+++ back/src/model.py
@@ -5,7 +5,7 @@ from models.training_scripts.model_utils import infer
 
 class Model:
     def __init__(self, model_path, faeture_groups: FeatureGroups):
-        self.model = joblib(model_path)
+        self.model = joblib.load(model_path)
         self.feature_group = faeture_groups
 
     def get_predictions(self, image_path):
